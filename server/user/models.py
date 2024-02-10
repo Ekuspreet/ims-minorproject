226cbc75dc8fec85e6d04b92460398f0220466c2
Dilraj Singh <dilraj082@gmail.com>
2024-02-10 22:11:26 +0530

    adding items to db

--- server/user/models.py
+++ server/user/models.py
@@ -14,8 +14,6 @@ class User:
     
     def signup(self):
         
-        print(request.json)
-        
         # create user object
         user = {
             "_id": uuid.uuid4().hex,
@@ -47,10 +45,10 @@ class User:
     def login(self):
         
         user = db.users.find_one({
-            "email": request.form.get('email')
+            "email": request.json["email"]
         })
         
-        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+        if user and pbkdf2_sha256.verify(request.json["password"], user['password']):
             return self.start_session(user)
         
         return jsonify({
