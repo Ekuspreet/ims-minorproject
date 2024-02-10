1cc7d0b9bf2e639ac91ba52b7719ebec692818b6
Dilraj Singh <dilraj082@gmail.com>
2024-02-10 16:00:34 +0530

    corrected return values

--- server/user/routes.py
+++ server/user/routes.py
@@ -5,13 +5,6 @@ from user.models import User
 @app.route('/user/signup', methods=["POST"])
 def signup():
     user = User()
-    
-    user_data, status_code = user.signup()
-    
-    if status_code == 200:
-        # If sign-up is successful, redirect to the dashboard route
-        return redirect("/dashboard")
-    
     return user.signup()
 
 @app.route('/user/signout')
@@ -20,8 +13,7 @@ def signout():
 
 @app.route('/user/login', methods=["POST"])
 def login():
-    user = User().login()
-    return render_template("dashboard.html")
+    return User().login()
 
 @app.route('/user/delete', methods=["POST"])
 def delete():
