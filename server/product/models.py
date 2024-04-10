34e7091b12b1ecb6f828004a8f76384b178bc4f5
dilrajs616 <dilraj082@gmail.com>
2024-04-10 23:47:00 +0530

    recipe started

--- server/product/models.py
+++ server/product/models.py
@@ -1,35 +1,55 @@
-from flask import request, jsonify
+from flask import request, jsonify, session
 from app import db
-from cryptography.fernet import Fernet
 import os
-
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
 
 class Product():
     
-    def add_product(self, business_id):
+    def add_product(self):
         
-        product = request.json
+        business_id = session.get("business_id")
+        name = request.json("name")
+        batch_size = request.json("batch_size")
+
+        product = {
+            "name": name,
+            "batch_size": batch_size,
+            "items": []
+        }
         product["product_id"] = self.get_product_id(business_id)
         if product["product_id"] == None:
             return jsonify({"Error": "Invalid business Id"})
-        
-        items = product["items"]
-        del product["items"]
-
-        encrypted_items = {}
-
-        for name, value in items.items():
-            encrypted_name = cipher_suite.encrypt(name.encode())
-            encrypted_items[encrypted_name] = value
-
-        product["items"] = encrypted_items
 
         if  db.businesses.update_one({"_id": business_id}, {"$push" :{"products": product}}):
             return jsonify({"success": True, "Message": "Product added successfully."})
         
         return jsonify({"success": False, "Message": "Product could not be added"})
+
+    def product_item():
+        business_id = session.get("business_id")
+        if not business_id:
+            return jsonify({"success": False, "error": "Business ID not found in session."}), 400
+        
+        product_id = request.json.get("product_id")
+        item_id = request.json.get("item_id")
+        quantity = float(request.json.get("quantity", 0))
+        
+        if not (product_id and item_id):
+            return jsonify({"success": False, "error": "Product ID or Item ID missing in request."}), 400
+        
+        item = {
+            "item_id": item_id,
+            "quantity": quantity
+        }
+        
+        result = db.businesses.update_one(
+            {"_id": business_id, "products.product_id": product_id},
+            {"$push": {"products.$.items": item}}
+        )
+        
+        if result.modified_count == 1:
+            return jsonify({"success": True, "message": "Item added successfully."})
+        else:
+            return jsonify({"success": False, "error": "Failed to add item to product."}), 500
 
 
     def get_product_id(self, business_id):
--- server/product/routes.py
+++ server/product/routes.py
@@ -1,6 +1,10 @@
 from app import app
 from product.models import Product
 
-@app.route("/product/<business_id>/add", methods = ["POST"])
+@app.route("/product/add", methods = ["POST"])
 def add_product(business_id):
-    return Product().add_product(business_id)
+    return Product().add_product()
+
+@app.route("/product/add_item")
+def product_item():
+    return Product.product_item()
