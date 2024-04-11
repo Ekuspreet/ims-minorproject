ed2166a760e2ed2a8c09117f9c1dfdd249921d16
dilrajs616 <dilraj082@gmail.com>
2024-04-12 00:30:13 +0530

    completed job section

--- server/job/models.py
+++ server/job/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class Job:
 
-    def fetch_active_jobs(self):
+    def fetch_jobs(self):
         business_id = session.get("business_id")
         business = db.businesses.find_one({"_id": business_id})
         jobs = business.get("jobs")
@@ -61,13 +61,63 @@ class Job:
         business_id = session.get("business_id")
         job_id = request.json.get("job_id")
 
+        business = db.businesses.find_one({"_id": business_id})
+        job = None
+        job_list = business["jobs"]
+        for j in job_list:
+            if j["job_id"] == job_id:
+                job = j
+                break
+
+        if not job:
+            return jsonify({"success": False, "message": "Bad JobID"})
+
+        product_id = job["product_id"]
+        product = None
+        for prod in business["products"]:
+            if prod["product_id"] == product_id:
+                product = prod
+                break
+
+        if not product:
+            return jsonify({"success": False, "message": "couldn't find product"})
+        
+        quantity = job["quantity"]
+        quantity_in_kg = int(quantity) * 50
+        batch_size = product["batch_size"]
+
+        for prod_item in product["items"]:
+            item_id = prod_item["item_id"]
+            required_stock = prod_item["quantity"]
+            item = None
+            for db_item in business["items"]:
+                if db_item["item_id"] == item_id:
+                    item = db_item
+                    break
+            if item:
+                available_stock = item["current_stock"]
+                new_stock = available_stock - ((required_stock/batch_size) * quantity_in_kg)
+                db.businesses.update_one({"_id": business_id, "items.item_id": item_id}, {"$set": {"items.$.current_stock": new_stock}})
+
+
         result = db.businesses.update_one({"_id": business_id, "jobs.job_id": job_id}, {"$set": {"jobs.$.status": "finish"}})
 
         if result.modified_count == 1:
-            return jsonify({"success": True, "message": "Job deleted successfully."})
+            return jsonify({"success": True, "message": "Job finished successfully."})
         
         else:
-            return jsonify({"success": False, "message": "Failed to delete job"})
+            return jsonify({"success": False, "message": "Failed to complete job"})
+
+    def fetch_complete_jobs(self):
+        business_id = session.get("business_id")
+        business = db.businesses.find_one({"_id": business_id})
+        jobs = business.get("jobs")
+        finished_jobs = []
+        for job in jobs:
+            if job["status"] == "finished":
+                finished_jobs.append(job)
+
+        return jsonify({"finished_jobs": finished_jobs})
 
     def get_job_id(self, business_id):
         business = db.businesses.find_one({"_id": business_id})
--- server/job/routes.py
+++ server/job/routes.py
@@ -1,6 +1,10 @@
 from app import app
 from job.models import Job
 
+@app.route("/job/fetch")
+def fetch_job():
+    return Job().fetch_jobs()
+
 @app.route("/job/create", methods=["POST"])
 def create_job():
     return Job().create_job()
@@ -8,3 +12,15 @@ def create_job():
 @app.route("/job/cancel", methods=["POST"])
 def cancel_job():
     return Job().cancel_job()
+
+@app.route("/job/start", methods=["POST"])
+def start_job():
+    return Job().start_job()
+
+@app.route("/job/finish", methods=["POST"])
+def finish_job():
+    return Job().finish_job()
+
+@app.route("/job/fetch/finished-jobs")
+def complete_jobs():
+    return Job().fetch_complete_jobs()
